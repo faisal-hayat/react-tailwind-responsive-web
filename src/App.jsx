62023f78093af6332c7467999687bd0ffc95d21a
M Faisal Hayat <m.faisalhayat3@gmail.com>
2023-06-21 00:42:09 +0500

    components has been created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
+// imports
 import React from 'react'
+import styles from "./style";
 
 function App() {
   return (
-    <div>App</div>
+    <div className="bg-primary w-full overflow-hidden">
+      {/* navbar */ }
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+      {/* hero section */ }
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero Components
+        </div>
+      </div>
+      {/* stats section */ }
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Bussiness
+          Billing
+          CardDeal
+          Testimonials
+          Clients
+          CTA
+          Footer
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/feedbackCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function feedbackCard() {
+  return (
+    <div>feedbackCard</div>
+  )
+}
+
+export default feedbackCard;
